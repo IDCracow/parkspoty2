39a58a1e333225093d9a3d9d7b68fd3ec281170f
Tomek Pilat <tomekpilat@gmail.com>
2015-04-14 13:06:23 +0200

    Initial commit 2

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name parkspotyappApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the parkspotyappApp
+ */
+angular.module('parkspotyappApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name parkspotyappApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the parkspotyappApp
+ */
+angular.module('parkspotyappApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
