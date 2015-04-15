68ea95185cb99090a03e1381dd8f2832e26d21fc
Marcin Ziolek <mar.ziolek@gmail.com>
2015-04-15 16:56:25 +0200

    login controller improvements

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -8,32 +8,36 @@
  * Controller of the parkspotyappApp
  */
 angular.module('parkspotyappApp')
-    .controller('LoginCtrl', function ($scope) {
+    .controller('LoginCtrl', function($scope, $location) {
     $scope.awesomeThings = [
         'HTML5 Boilerplate',
         'AngularJS',
         'Karma'
     ];
-})
-    .run(['$rootScope', function($scope) {
-        $scope.currentUser = Parse.User.current();
 
-        $scope.logIn = function(form) {
-            Parse.User.logIn(form.username, form.password, {
-                success: function(user) {
-                    $scope.currentUser = user;
-                    $scope.$apply();
-                    console.log($scope.currentUser);
-                },
-                error: function(user, error) {
-                    alert("Unable to log in: " + error.code + " " + error.message);
-                }
-            });
+    $scope.currentUser = Parse.User.current();
 
-        };
+    $scope.logIn = function(form) {
+        Parse.User.logIn(form.username, form.password, {
+            success: function(user) {
+                $scope.currentUser = user;
+                $scope.goToUserProfile();
+                $scope.$apply();
+            },
+            error: function(user, error) {
+                alert("Unable to log in: " + error.code + " " + error.message);
+            }
+        });
 
-        $scope.logOut = function(form) {
-            Parse.User.logOut();
-            $scope.currentUser = null;
-        };
-    }]);
+    };
+
+    $scope.logOut = function(form) {
+        Parse.User.logOut();
+        $scope.currentUser = null;
+    };
+
+    $scope.goToUserProfile = function() {
+        $location.path('/about');
+        console.log($location);
+    };
+});
