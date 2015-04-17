ea9363415a614a9b4bd247b1027ddfb0ef8a50d2
Marcin Ziolek <mar.ziolek@gmail.com>
2015-04-17 14:28:36 +0200

    #10 update cloud code

--- parseDBparkspoty2/cloud/main.js
+++ parseDBparkspoty2/cloud/main.js
@@ -1,17 +1,16 @@
+//email validation
+Parse.Cloud.beforeSave("user", function(request, response) {
+    var emailAddress = request.object.get("email");
+    var domain = emailAddress.split('@')[1];
+    if (domain.toLowerCase() === "infusion.com") {
+        response.success();
+    } else {
+        response.error("Please use your company email address.");
+    };
+});
 
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
-});
-
-//email validation
-Parse.Cloud.beforeSave("user", function(req, res) {
-    var emailAddress = req.object.get("email");
-    var domain = emailAddress.split('@')[1];
-    if (domain.toLowerCase() === "infusion.com") {
-        res.success();
-    } else {
-        res.error("Please use your company email address.");
-    };
 });
