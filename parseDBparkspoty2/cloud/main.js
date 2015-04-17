ee2da0ed82146b6285020bd9463f0576cc3e9015
Marcin Ziolek <mar.ziolek@gmail.com>
2015-04-17 14:26:35 +0200

    #10 basic validation for registration TEST

--- parseDBparkspoty2/cloud/main.js
+++ parseDBparkspoty2/cloud/main.js
@@ -4,3 +4,14 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+//email validation
+Parse.Cloud.beforeSave("user", function(req, res) {
+    var emailAddress = req.object.get("email");
+    var domain = emailAddress.split('@')[1];
+    if (domain.toLowerCase() === "infusion.com") {
+        res.success();
+    } else {
+        res.error("Please use your company email address.");
+    };
+});
