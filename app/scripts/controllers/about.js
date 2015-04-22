b36cd48cfcaf8f85092d1185f709c6751a6950e6
Monika Zamojska <mzamojska@infusion.com>
2015-04-22 14:40:23 +0200

    Weather widget #3

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -10,4 +10,5 @@
 angular.module('parkspotyappApp')
   .controller('AboutCtrl', function ($scope,weatherService) {
     
+    
   });
--- app/scripts/directives/weatherDirective.js
+++ app/scripts/directives/weatherDirective.js
@@ -1,12 +1,13 @@
-angular.module('parkspotyappApp').directive('weatherTemp', function() {
+angular.module('parkspotyappApp').directive('weatherInfo', function() {
     return {
         restrict: 'E', replace: true,
         controller: function($scope,weatherService) {
               weatherService.getWeather(function(data) {
+                  console.log(data);
                  $scope.weatherDataTemp = data.main.temp;
+                 $scope.weatherDataIcon = 'icon-' + data.weather[0].icon;
               });
-
         },
-        template: '<div>{{weatherDataTemp}}</div>'
+        template: "<div><span>Kraków,</span> <span>{{weatherDataTemp}}</span><span class='{{weatherDataIcon}}'></span></div>"
     };
 });
